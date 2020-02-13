ce63aae591990850cab17b06cee85c7639236263
zhanglei <zhanglei@xiaoshouyi.com>
2020-02-13 22:57:12 +0800

    up

---
+++ test/test-case1.js
@@ -0,0 +1,32 @@
+/** comments ****/
+
+var a=10; /****/
+
+/**
+ contents
+ contents
+ contents
+**/  var a=10;
+
+/**
+     contents
+    contents
+    contents
+    **/  var a=10;
+
+    var a=10;   /**
+     contents
+    contents
+    contents
+**/   var a=10;
+
+ var a=10;   /**
+     contents
+    contents
+ contents
+**/
+
+//comments
+var a=10;  //comments
+   //comments
+   
